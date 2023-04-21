0843ac004b625ca92b163cff1eaf02500d00f228
valentinaarojas <valentinaxrojas@gmail.com>
2023-04-21 14:10:50 -0400

    Updates

---
+++ WordScramble.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/* 
+ * This is a word scramble game. 
+ */
+
+public class WordScramble {
+
+    private Input input = new Input();
+    private String guess;
+    private 
+
+    public void PlayGame() {
+        input.sc.nextInt();
+    }
+
+
+}
---
+++ WordScrambleData.java
@@ -0,0 +1,9 @@
+public class WordScrambleData {
+    
+    public String[] WordList() {
+        String[] scrambledWords = {"sunshine", "elephant", "bicycle", "telephone", "chocolate",
+                                   "computer", "mountain", "butterfly", "rainbow", "guitar"};
+        return scrambledWords;
+    }
+
+ }
