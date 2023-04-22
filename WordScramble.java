1dd5a9dd2ac84e04d1cf04c060af6b0318d3f568
valentinaarojas <valentinaxrojas@gmail.com>
2023-04-22 17:47:23 -0400

    Update WordScramble.java

--- WordScramble.java
+++ WordScramble.java
@@ -1,18 +1,41 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 /* 
  * This is a word scramble game. 
  */
 
-public class WordScramble {
+public class WordScramble extends WordScrambleData {
 
-    private Input input = new Input();
-    private String guess;
-    private 
+    private String playerGuess;
+    private List<String> wordsToGuess = new ArrayList<>();
+    private List<String> scrambledWords = new ArrayList<>();
+    private int amtWordsLeft = 0;
+    private boolean playAgain;
 
-    public void PlayGame() {
-        input.sc.nextInt();
+    public void PlayGame() throws InterruptedException {
+        do {
+            playAgain = true;
+            System.out.println("\n\t ~~ Welcome to a game of Word Scramble! ~~");
+            PrintRules();
+            SelectDifficulty();
+
+            System.out.print("\nAcquiring words");
+            input.Delay("...", 500);
+            amtWordsLeft = wordsToGuess.size();
+            scrambledWords = ScrambleWords(wordsToGuess);
+
+            do {
+                StartGame();
+            } while (amtWordsLeft > 0);
+
+            ShowWords(scrambledWords);
+            System.out.println("\n\nCongrats! You Won ~ *");
+
+            playAgain = input.PlayAgain();
+        } while (playAgain);
     }
 
 
+    
 }
