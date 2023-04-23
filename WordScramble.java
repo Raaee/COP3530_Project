e03f4dca9416123c94a765c465fd14fee89521f7
valentinaarojas <valentinaxrojas@gmail.com>
2023-04-22 21:28:11 -0400

    Completed Data File for WordScramble

--- WordScramble.java
+++ WordScramble.java
@@ -36,6 +36,80 @@ public class WordScramble extends WordScrambleData {
         } while (playAgain);
     }
 
+    public void StartGame() {
+        System.out.println("\n");
+        System.out.println("Words Left: " + amtWordsLeft);
+        // ShowWords(wordsToGuess); // DEBUG
+        ShowWords(scrambledWords);
+        EvaluateGuess();
+    }
 
-    
+    public void EvaluateGuess() {
+        boolean loop;
+        int index;
+        String wordGuessing;
+
+        do {
+            index = 0;
+            loop = false;
+            System.out.println("\nEnter Guess: Format >>(1 myguess)");
+            playerGuess = input.sc.nextLine();
+            playerGuess = playerGuess.replaceAll("\\s", "");
+
+            if (playerGuess.equalsIgnoreCase("Rules") || playerGuess.contains("rules")) {
+                loop = false;
+            } else {
+                if (Character.isDigit(playerGuess.charAt(0))) {
+                    index = Integer.parseInt(String.valueOf(playerGuess.charAt(0)));
+                }
+                if (!(playerGuess.matches("\\d+.+"))) {
+                    System.out.println("x ~ Incorrect Formatting.");
+                    loop = true;
+                }
+                if (index < 1 || index > wordsToGuess.size()) {
+                    System.out.println("x ~ That is not a valid word number.");
+                    loop = true;
+                }
+            }
+        } while (loop);
+
+        playerGuess = playerGuess.toUpperCase();
+        switch (playerGuess) {
+            case "RULES" -> PrintRules();
+            default -> {
+                index -= 1;
+                wordGuessing = wordsToGuess.get(index);
+                playerGuess = playerGuess.substring((playerGuess.charAt(1) == ' ') ? 2 : 1, playerGuess.length());
+
+                if (playerGuess.equalsIgnoreCase(wordGuessing)) {
+                    System.out.println("Correct!");
+                    scrambledWords.set(index, wordsToGuess.get(index) + " ~");
+                    amtWordsLeft--;
+                } else {
+                    System.out.println("Try Again");
+                }
+            }
+        }
+    }
+
+    public void SelectDifficulty() {
+        switch (input.DifficultyMenu()) {
+            case "1" -> wordsToGuess = AddWords(2);
+            case "2" -> wordsToGuess = AddWords(4);
+            case "3" -> wordsToGuess = AddWords(6);
+        }
+    }
+
+    public void PrintRules() {
+        System.out.println("\nRules:");
+        System.out.println("------------");
+        System.out.println("> You will be given a set of words with its letters scrambled.");
+        System.out.println("> The goal is to guess the correct words by descrambling the letters.");
+
+        System.out.println("\n> To guess a specific word, indicate the number and your guess.");
+        System.out.println("> Input example: (1 myguess) OR (1myguess)");
+        System.out.println("** If at anytime you wish to see the rules again, type in \"Rules\" **");
+        System.out.println("Good luck!");
+    }
+
 }
--- WordScrambleData.java
+++ WordScrambleData.java
@@ -1,9 +1,51 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class WordScrambleData {
-    
-    public String[] WordList() {
-        String[] scrambledWords = {"sunshine", "elephant", "bicycle", "telephone", "chocolate",
-                                   "computer", "mountain", "butterfly", "rainbow", "guitar"};
+
+    public Input input = new Input();
+
+    private String[] words = { "sunshine", "elephant", "bicycle", "telephone", "chocolate",
+            "computer", "mountain", "butterfly", "rainbow", "guitar",
+            "bigmatic", "quagmire", "funny", "bright", "debt", "inevitable", "paradox",
+            "magnanimous", "enfarious", "disseminate", "frivolous", "luminous", "pernicious" };
+
+    public String[] GetWordList() {
+        return words;
+    }
+
+    public void ShowWords(List<String> words) {
+        System.out.println("--------------");
+        for (int i = 0; i < words.size(); i++) {
+            System.out.println((i + 1) + ". " + words.get(i));
+        }
+    }
+
+    public List<String> AddWords(int amtWords) {
+        List<String> wordsToGuess = new ArrayList<>();
+
+        for (int i = 0; i < amtWords; i++) {
+            int randomIndx = input.GetRandomNum(0, GetWordList().length - 1);
+            wordsToGuess.add(GetWordList()[randomIndx]);
+        }
+        return wordsToGuess;
+    }
+
+    public List<String> ScrambleWords(List<String> words) {
+        List<String> scrambledWords = new ArrayList<>();
+
+        for (int i = 0; i < words.size(); i++) {
+            char[] chars = words.get(i).toCharArray();
+
+            for (int n = 0; n < chars.length; n++) {
+                int ranIndx = input.GetRandomNum(0, chars.length - 1);
+                char temp = chars[i];
+                chars[i] = chars[ranIndx];
+                chars[ranIndx] = temp;
+            }
+            scrambledWords.add(new String(chars));
+        }
         return scrambledWords;
     }
 
- }
+}
